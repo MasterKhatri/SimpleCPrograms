22716b4f5c79206f4412963d4c6f50d60b06430a
Akash Khatri <akash.khatri2000@gmail.com>
2019-01-26 15:47:21 +0530

    Update matrixTranspose.c

--- matrixTranspose.c
+++ matrixTranspose.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-void matrixTranspose(int a[][10], int r, int c);
-void main()  
-{
-	int x, y, i, j;
-	printf("Enter rows and columns of matrix\n");
-	scanf("%d %d", &x, &y);
-	int b[x][y];
-	printf("Enter elements of matrix\n");
-	for(i=0; i<x; i++)
-		for(j=0; j<y; j++)
-			scanf("%d", &b[i][j]);
-	matrixTranspose(b, x, y);
-}
-
 void matrixTranspose(int a[][10], int r, int c) {
 	int mat[10][10]= {0,0}, i, j;
 	for(i=0; i<r; i++)
@@ -23,6 +9,20 @@ void matrixTranspose(int a[][10], int r, int c) {
 			printf("%d\n", mat[i][j]);
 		}
 	}
+}
+
+main()  
+{
+	int x, y, i, j;
+	printf("This simple program prints the transpose of a matrix\n");
+	printf("Enter rows and columns of matrix\n");
+	scanf("%d %d", &x, &y);
+	int b[x][y];
+	printf("Enter elements of matrix\n");
+	for(i=0; i<x; i++)
+		for(j=0; j<y; j++)
+			scanf("%d", &b[i][j]);
+	matrixTranspose(b, x, y);
 }
 
 
