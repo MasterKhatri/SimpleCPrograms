3a34c5a16d683d90d43791958107e10655b3387a
Akash Khatri <akash.khatri2000@gmail.com>
2019-01-26 12:51:26 +0530

    Update and rename factor.c to factorization.c

--- factorization.c
+++ factorization.c
@@ -2,10 +2,11 @@
 main()
 {
 	int n, i;
+	printf("This simple program prints all the factors of the given number\n");
 	printf("Enter a number\n");
 	scanf("%d", &n);
 	for(i=1; i<=n; i++) {
-		if(n%i==0) {
+		if(n%i == 0) {
 			printf("%d, ", i);
 		}
 	}
