5981f1836b58e44f97adaa94c27270d9ff1c2ad5
Akash Khatri <akash.khatri2000@gmail.com>
2019-02-04 14:44:48 +0530

    copyString: Add preface and spacing

--- copyString.c
+++ copyString.c
@@ -3,14 +3,15 @@ void main()
 {
 	int i = 0;
 	char name1[20], name2[20];
-	printf("Enter string ");
+	printf("This simple program copies given string to another string; same as strcpy\n");
+	printf("Enter string: \n");
 	gets(name1);
 	while (name1[i] != '\0') {
-		name2[i]=name1[i];
+		name2[i] = name1[i];
 		i++;
 	}
-	name2[i]='\0';
-	printf("%s",name2);
+	name2[i] = '\0';
+	printf("%s", name2);
 }
 
 
