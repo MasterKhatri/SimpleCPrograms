d18e3ac8c1dc2a8af4b54d0546c0ab8ee1b2390a
Akash Khatri <akash.khatri2000@gmail.com>
2019-02-04 14:37:10 +0530

    convertMatrix: Add preface

--- convertMatrix.c
+++ convertMatrix.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+void main()
 {
 	int a[4][4];
-	for(int i=0;i<4;i++)
+	printf("This simple program converts a 3*3 matrix into 4*4\n");
+	printf("Enter a 3*3 matrix\n");
+	for(int i=0; i<4; i++)
 	{
-		for(int j=0;j<4;j++)
+		for(int j=0; j<4; j++)
 		{
-			if(i!=3&& j!=3)
-				scanf("%d",&a[i][j] );
+			if(i!=3 && j!=3)
+				scanf("%d", &a[i][j] );
 			else
-				a[i][j]=i*j;
+				a[i][j] = i * j;
 		}
 	}
-	for(int i=0;i<4;i++)
+	for(int i=0; i<4; i++)
 	{
-		for(int j=0;j<4;j++)
-			printf("%d ",a[i][j]);
+		for(int j=0; j<4; j++)
+			printf("%d ", a[i][j]);
 		printf("\n");
 	}
 }
