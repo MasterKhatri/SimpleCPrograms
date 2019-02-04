f5f97d6b90f1a6137bbf8031a01cb6e432a1e970
Akash Khatri <akash.khatri2000@gmail.com>
2019-02-04 10:45:44 +0530

    armstrongNumber: Add preface and spaces

--- armstrongNumber.c
+++ armstrongNumber.c
@@ -2,13 +2,14 @@
 #include <math.h>
 void main() 
 {
-	int n, sum=0;
+	int n, sum = 0;
+	printf("This simple program checks if a number is armstrong number\n");
 	printf("Enter number to be checked for armstrong\n");
 	scanf("%d", &n);
-	while(n>0){
-		sum=n%10;
-		sum+=pow(n,3);
-		n/=10;
+	while(n > 0){
+		sum = n % 10;
+		sum += pow(n, 3);
+		n /= 10;
 	}
 	printf("%d", sum);
 }
