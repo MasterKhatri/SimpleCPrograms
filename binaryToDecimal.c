a45f56324a116c15a0507609c1ce8dc49de34953
Akash Khatri <akash.khatri2000@gmail.com>
2019-02-04 10:47:46 +0530

    binaryToDecimal: Add preface and fix derps

--- binaryToDecimal.c
+++ binaryToDecimal.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-int main()
+void main()
 {
 
 	long long n; 
 	long long sum=0, i=0;
+	printf("This simple program converts a binary number to decimal number\n");
 	printf("Enter a binary number\n");
 	scanf("%d", &n);
 	while(n){
-		sum+= pow(2,i)*(n%10);
-		n/= 10;
+		sum += pow(2, i)*(n % 10);
+		n /= 10;
 		i++;
 	}
 			printf("%d", sum);
