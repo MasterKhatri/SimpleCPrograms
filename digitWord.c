e05d47b14795db5016ce52dd9654155d112f8660
Akash Khatri <akash.khatri2000@gmail.com>
2019-02-13 02:29:01 +0530

    digitWord: Add preface

--- digitWord.c
+++ digitWord.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 void main()
 {
-	int n, rev=0, rem;
+	int n, rev = 0, rem;
+	printf("This simple program prints the reverse of the given number\n");
 	printf("Enter digits\n");
 	scanf("%d", &n);
 	while(n>0) {
-		rem=n%10;
-		rev=rev*rem;
-		n/=10;
+		rem = n % 10;
+		rev = rev * rem;
+		n / = 10;
 	}
 
 	printf("%d", rev);
