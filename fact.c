23da33a0a6d2c11bf6e481b87118a2ebf7723f43
Akash Khatri <akash.khatri2000@gmail.com>
2019-02-13 02:32:11 +0530

    fact: Add preface

--- fact.c
+++ fact.c
@@ -3,15 +3,16 @@ int fact(int a);
 void main()
 {
 	int x, factorial=0;
+	printf("This simple program calculates the factorial of a given number\n");
 	printf("Enter a number\n");
 	scanf("%d", &x);
 	factorial = fact(x);
 	printf("%d", factorial);
 }
 int fact(int a) {
-	int i, f=1;
+	int i, f = 1;
 	for(i=1; i<=a; i++)
-		f*=i;
+		f *= i;
 	return f;
 }
 
