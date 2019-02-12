8db564a3dfa1e9d32584e3f0795551577c99d277
Akash Khatri <akash.khatri2000@gmail.com>
2019-02-13 02:35:13 +0530

    factorsRecursion: Add preface and fix data type

--- factorsRecursion.c
+++ factorsRecursion.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-void primefactor(int );
-main()
+void primefactor(int);
+void main()
 {
 	int a;
+	printf("This simple program prints the prime factors of a given number, using Recursion logic \n");
 	printf("Enter a number \n");
 	scanf("%d", &a);
 	primefactor(a);
 }
 
 void primefactor(int n) {
-	static int i=1;
+	static int i = 1;
 	if(i<=n) { 
 		if(n%i==0) {
 			printf("%d", i);
